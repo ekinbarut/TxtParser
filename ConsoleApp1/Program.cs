b7e1879d6d1f7b20820c54d9a70fd9b569ca944a
Ekin Barut <ekinbarut@gmail.com>
2020-05-20 18:06:51 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> text = new List<string>();
+            string inputPath = "D:/Storage/Test.txt";
+            string outputPath = "D:/Storage/Ekin.txt";
+            using (StreamReader sr = File.OpenText(inputPath))
+            {
+                string s = String.Empty;
+                string temp = String.Empty;
+                while ((temp = sr.ReadLine()) != null)
+                {
+                    if (temp != s)
+                    {
+                        text.Add(s);
+                        s = temp;
+                    }
+                }
+            }
+            text = text.Distinct().ToList();
+
+            File.WriteAllLines(outputPath, text);
+
+            
+        }
+
+    }
+}
